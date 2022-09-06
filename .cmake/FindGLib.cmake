f8bd8283631004c05748ef8682887302af72c6df
Bahman Movaqar <bahman@bahmanm.com>
2022-09-06 10:20:44 -0700

    project skeleton w/ glib

---
+++ .cmake/FindGLib.cmake
@@ -0,0 +1,4 @@
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(GLIB REQUIRED glib-2.0)
+include_directories(${GLIB_INCLUDE_DIRS})
+link_directories(${GLIB_LIBRARY_DIRS})
---
+++ .cmake/reformat.cmake
@@ -0,0 +1,14 @@
+##########
+# enforce formatting via astyle.
+# the style is configured in ${PROJECT_SOURCE_DIR}/.astyle
+##########
+file(
+  GLOB_RECURSE MY_SOURCE_FILES
+  LIST_DIRECTORIES false
+  ${PROJECT_SOURCE_DIR}/src/*.[ch])
+
+foreach(file ${MY_SOURCE_FILES})
+  message(STATUS "Formatting ${file}...")
+  execute_process(
+    COMMAND astyle --options=${PROJECT_SOURCE_DIR}/.astylerc ${file})
+endforeach()
