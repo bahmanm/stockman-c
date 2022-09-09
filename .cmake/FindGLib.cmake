31ded98f9ea0e3b7ed325860d1346ae6a3219dde
Bahman Movaqar <bahman@bahmanm.com>
2022-09-09 07:03:45 -0700

    add gio libs

--- .cmake/FindGLib.cmake
+++ .cmake/FindGLib.cmake
@@ -1,4 +1,9 @@
 find_package(PkgConfig REQUIRED)
+
 pkg_check_modules(GLIB REQUIRED glib-2.0)
 include_directories(${GLIB_INCLUDE_DIRS})
 link_directories(${GLIB_LIBRARY_DIRS})
+
+pkg_check_modules(GIO REQUIRED gio-2.0)
+include_directories(${GIO_INCLUDE_DIRS})
+link_directories(${GIO_LIBRARY_DIRS})
