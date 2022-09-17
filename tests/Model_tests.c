83411d68b7b6aff24d7f0d1eb11ea4528e5d18b3
Bahman Movaqar <bahman@bahmanm.com>
2022-09-17 07:49:56 -0700

    rewrite the tests using the new api

--- tests/Model_tests.c
+++ tests/Model_tests.c
@@ -17,56 +17,66 @@
  * along with Stockman-C. If not, see <https://www.gnu.org/licenses/>.
  */
 #include <glib.h>
-#include "Model.h"
+#include "model/invoice.h"
+#include "model/invoiceline.h"
+#include "model/model.h"
 
 void
-test_Invoice_addLine()
+test_stk_model_invoice_add_line()
 {
-	// GIVEN
-	Model_Invoice inv = {.lines = NULL};
-	Model_InvoiceLine iline1 = {.line_no = 10, .product = "P1"};
-	Model_InvoiceLine iline2 = {.line_no = 20, .product = "P2"};
+	/* GIVEN */
+	g_autoptr(Stk_Model_Invoice) inv = stk_model_invoice_new();
 
-	// EXPECT
-	g_assert_null(inv.lines);
+	/* EXPECT */
+	g_assert_null(stk_model_invoice_get_lines(inv));
 
-	// WHEN
-	Model_Invoice_addLine(&inv, &iline1);
+	/* WHEN */
+	g_autoptr(Stk_Model_InvoiceLine) iline1 = stk_model_invoiceline_new();
+	stk_model_invoiceline_set_line_no(iline1, 10);
+	stk_model_invoiceline_set_product(iline1, "P1");
+	stk_model_invoice_add_line(inv, iline1);
 
-	// THEN
-	g_assert_nonnull(inv.lines);
-	g_assert_cmpint(1, ==, g_list_length(inv.lines));
-	g_assert_nonnull(g_list_find(inv.lines, &iline1));
+	/* THEN */
+	g_autoptr(GList) lines1 = stk_model_invoice_get_lines(inv);
+	g_assert_nonnull(lines1);
+	g_assert_cmpint(1, ==, g_list_length(lines1));
+	g_assert_nonnull(g_list_find(lines1, iline1));
 
-	// WHEN
-	Model_Invoice_addLine(&inv, &iline2);
+	/* WHEN */
+	g_autoptr(Stk_Model_InvoiceLine)  iline2 = stk_model_invoiceline_new();
+	stk_model_invoiceline_set_line_no(iline2, 20);
+	stk_model_invoiceline_set_product(iline2, "P2");
+	stk_model_invoice_add_line(inv, iline2);
 
-	// THEN
-	g_assert_cmpint(2, ==, g_list_length(inv.lines));
-	g_assert_nonnull(g_list_find(inv.lines, &iline1));
-	g_assert_nonnull(g_list_find(inv.lines, &iline2));
+	/* THEN */
+	g_autoptr(GList) lines2 = stk_model_invoice_get_lines(inv);
+	g_assert_cmpint(2, ==, g_list_length(lines2));
+	g_assert_nonnull(g_list_find(lines2, iline1));
+	g_assert_nonnull(g_list_find(lines2, iline2));
 }
 
 void
-test_InvoiceLine_compareByLineNo()
+test_stk_model_invoiceline_compare_by_line_no()
 {
-	// GIVEN
-	Model_InvoiceLine iline1 = {.line_no = 10};
-	Model_InvoiceLine iline2 = {.line_no = 20};
+	/* GIVEN */
+	g_autoptr(Stk_Model_InvoiceLine) iline1 = stk_model_invoiceline_new();
+	stk_model_invoiceline_set_line_no(iline1, 10);
+	g_autoptr(Stk_Model_InvoiceLine) iline2 = stk_model_invoiceline_new();
+	stk_model_invoiceline_set_line_no(iline2, 20);
 
-	// EXPECT
-	g_assert_cmpint(0, ==, Model_InvoiceLine_compareByLineNo(&iline1, &iline1));
-	g_assert_cmpint(0, >, Model_InvoiceLine_compareByLineNo(&iline1, &iline2));
-	g_assert_cmpint(0, <, Model_InvoiceLine_compareByLineNo(&iline2, &iline1));
+	/* EXPECT */
+	g_assert_cmpint(0, ==, stk_model_invoiceline_compare_by_line_no(iline1, iline1));
+	g_assert_cmpint(0, >, stk_model_invoiceline_compare_by_line_no(iline1, iline2));
+	g_assert_cmpint(0, <, stk_model_invoiceline_compare_by_line_no(iline2, iline1));
 }
 
 int
 main(int argc, char **argv)
 {
 	g_test_init(&argc, &argv, NULL);
-	g_test_add_func("/models/Invoice/addLine",
-	                test_Invoice_addLine);
-	g_test_add_func("/models/InvoiceLine/compareByLineNo",
-	                test_InvoiceLine_compareByLineNo);
+	g_test_add_func("/Model/Invoice/add_line",
+	                test_stk_model_invoice_add_line);
+	g_test_add_func("/Model/InvoiceLine/compare_by_line_no",
+	                test_stk_model_invoiceline_compare_by_line_no);
 	return g_test_run();
 }
